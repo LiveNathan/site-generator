8d6b2c564ef6fd7260086ced80c49e337e59a67c
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-08-14 09:50:23 -0500

    Refactor section creation logic in sections.js
    The logic for creating different types of sections (header, text, gallery, contact, links) for the COOKING_SITE in the sections.js file was refactored. The change involved introducing a utility function to create HTML elements and restructuring the section creation procedures into separate functions. The update improves code maintainability and makes the logic more scalable and flexible in anticipation of potential new types of sections in the future.

--- src/sections.js
+++ src/sections.js
@@ -1,70 +1,67 @@
-import { COOKING_SITE } from './index.js';
-function createHeader(section) {
-    const header = document.createElement('h2');
-    const headerText = section.header;
-    header.innerText = headerText;
-    header.id = headerText.toLowerCase().trim().replace(/\s+/g, '-').replace(/[^a-z0-9\-]/g, '')  // Replace white space with dash.
-    header.classList.add('header text-2xl font-bold');
+import {COOKING_SITE} from './index.js';
 
-    const imagesContainer = document.createElement('div');
-    imagesContainer.classList.add('');
-    section.images.forEach(imageUrl => {
-        const image = document.createElement('img');
+const createHTMLElement = (element, id, classes = []) => {
+    const htmlElement = document.createElement(element);
+    if (id) htmlElement.id = id.toLowerCase().trim().replace(/\s+/g, '-').replace(/[^a-z0-9\-]/g, '');
+    if (classes && classes.length > 0) classes.forEach(cls => htmlElement.classList.add(cls));
+    return htmlElement;
+}
+
+const createHeader = (section) => {
+    const headerContainer = createHTMLElement('div');
+    const header = createHTMLElement('h2', section.header, ['header', 'text-2xl', 'font-bold']);
+    header.innerText = section.header;
+    headerContainer.append(header);
+
+    if (section.hasOwnProperty('images') && section.images.length > 0) {
+        header.style.backgroundImage = `url(${section.images[0]})`;
+        if (section.images.length > 1) createGallery(section.images);
+    }
+
+    document.body.appendChild(headerContainer);
+}
+
+const createText = (section) => {
+    const textContainer = createHTMLElement('div');
+    const textHeader = createHTMLElement('h3', section.header);
+    textHeader.innerText = section.text;
+    textContainer.append(textHeader);
+
+    const text = createHTMLElement('p', section.header, ['text-sm']);
+    text.innerText = section.text;
+    textContainer.append(text);
+
+    document.body.appendChild(textContainer);
+}
+
+const createGallery = (images) => {
+    const imagesContainer = createHTMLElement('div');
+    images.forEach(imageUrl => {
+        const image = createHTMLElement('img');
         image.src = imageUrl;
         imagesContainer.appendChild(image);
-    })
-
-    document.body.appendChild(header);
+    });
+    document.body.appendChild(imagesContainer);
 }
 
-function createText(section) {
-    const text = document.createElement('p');
-    // text.id = 'main-header';
-    // text.classList.add('header');
-    text.innerText = 'Hello World';
-    document.body.appendChild(text);
+const createContact = (section) => {
+    // Logic for preparing Contact section will go here.
 }
 
-function createGallery(section) {
-    const gallery = document.createElement('img');
-    // gallery.id = 'main-header';
-    // gallery.classList.add('header');
-    // gallery.innerText = 'Hello World';
-    document.body.appendChild(gallery);
+const createLinks = (section) => {
+    // Logic for preparing Links section will go here.
 }
 
-function createContact(section) {
-    const contact = document.createElement('p');
-    // contact.id = 'main-header';
-    // contact.classList.add('header');
-    contact.innerText = 'Hello World';
-    document.body.appendChild(contact);
-}
-
-function createLinks(section) {
-    const links = document.createElement('a');
-    // links.id = 'main-header';
-    // links.classList.add('header');
-    links.innerText = 'Hello World';
-    document.body.appendChild(links);
-}
+const sectionCreators = {
+    'header': createHeader,
+    'text': createText,
+    'gallery': createGallery,
+    'contact': createContact,
+    'links': createLinks
+};
 
 COOKING_SITE.sections.forEach(section => {
-    switch (section.type) {
-        case 'header':
-            createHeader(section);
-            break;
-        case 'text':
-            createText(section);
-            break;
-        case 'gallery':
-            createGallery(section);
-            break;
-        case 'contact':
-            createContact(section);
-            break;
-        case 'links':
-            createLinks(section);
-            break;
+    if (sectionCreators[section.type]) {
+        sectionCreators[section.type](section);
     }
-})
+});
